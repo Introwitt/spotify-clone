0046d403714bbbc286ff1152c75fae5f9be304d4
Harsh Kashyap <harshkashyap307@gmail.com>
2020-12-03 11:12:11 +0530

    Spotify Authentication

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -1,13 +1,34 @@
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Login from './components/Login';
+import {getTokenFromUrl} from './spotify';
 
 function App() {
+
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if(_token){
+      setToken(_token);
+    }
+
+    console.log("I have a token 👉", _token);
+  }, [])
+
+
   return (
     // BEM
     <div className="app">
-      <header className="App-header">
-        <Login />
-      </header>
+    {
+      token ? <h1>Logged in!!!</h1>
+      :
+      <Login />
+    }
     </div>
   );
 }
