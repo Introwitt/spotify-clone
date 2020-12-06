80da9f226dbf89212294786f1c37b450017c5213
Harsh Kashyap <harshkashyap307@gmail.com>
2020-12-07 00:04:14 +0530

    Style the footer

--- spotify-clone-app/src/reducer.js
+++ spotify-clone-app/src/reducer.js
@@ -1,8 +1,8 @@
 export const initialState = {
     user: null,
     playlists: [],
-    // token: 'BQCv3rLOKmAM-yQkcEcVO9mURwjPhtKTWitUuEUAwXExFOP2IUQJMUEM49ie58DeQEy-yZAFvXAeD5qKs4Wde6h93WpXUhvd39goe3MuHIWS3htWuO6yTzTyVNRSGZ8OkGlnCPWaHsbFo6vzcKJKuxz-8p-WClfKfExl_lKSnnUiPRw4',
-    // playlists:[],
+    token: 'BQCv3rLOKmAM-yQkcEcVO9mURwjPhtKTWitUuEUAwXExFOP2IUQJMUEM49ie58DeQEy-yZAFvXAeD5qKs4Wde6h93WpXUhvd39goe3MuHIWS3htWuO6yTzTyVNRSGZ8OkGlnCPWaHsbFo6vzcKJKuxz-8p-WClfKfExl_lKSnnUiPRw4',
+    playlists:[],
 };
 
 const reducer = (state, action) => {
